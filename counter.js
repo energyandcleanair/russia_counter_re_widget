688902fee2fc858a701fa968fddf5ee5d4e8a720
Hubert Thieriot <hubert@energyandcleanair.org>
2022-06-02 17:41:39 +0530

    -first commit

--- counter.js
+++ counter.js
@@ -1,37 +1,3 @@
-const html_string = `
-    <div class='container'>
-        <div class='headers'>
-            <h1>Payments to Russia for fossil fuels</h1>
-            <div class='subtitle'>By European Union countries since 24 February 2022</div>
-        </div>
-        <div class='big-box'>
-            <span class='currency'>EUR</span>
-            <div class='counter' id='total_eur_per_sec'></div>
-        </div>
-        <div class='row'>
-            <div class='oc'>
-                <div class='small-box'>
-                    <div class='title'><span>Oil</span></div>
-                    <div class='o-number'><span class='currency'>EUR </span><span id='oil_eur'> </span><span> M</span></div>
-                </div>
-            </div>
-            <div class='oc'>
-                <div class='small-box'>
-                    <div class='title'><span>Gas</span></div>
-                    <div class='o-number'><span class='currency'>EUR </span><span id='gas_eur'> </span><span> M</span></div>
-                </div>
-            </div>
-            <div class='oc'>
-                <div class='small-box'>
-                    <div class='title'><span>Coal</span></div>
-                    <div class='o-number'><span class='currency'>EUR </span><span id='coal_eur'> </span><span> M</span></div>
-                </div>
-            </div>
-        </div>
-        <div class='caption'>
-            <p>Source: CREA analysis. See further results, methodology and updates <a href="https://crea.shinyapps.io/russia_counter/?tab=methodology" target='_blank'>here</a>.</p>
-        </div>
-    </div>`;
 
 const pull_data = async ()=>{
  let response = await fetch("https://api.russiafossiltracker.com/v0/counter_last?destination_region=EU&use_eu=True&aggregate_by=destination_region,commodity_group");
@@ -87,14 +53,9 @@ const update_ui = (data)=>{
 
 }
 
-const ui_setup = (elem_id)=>{
-    let head = document.getElementById(elem_id);
-    head.innerHTML = html_string;
-}
 
-const set_counter = (elem_id)=>{
+const set_counter = ()=>{
     document.addEventListener('DOMContentLoaded', (event) => {
         pull_data();    
     })
-    
 }
